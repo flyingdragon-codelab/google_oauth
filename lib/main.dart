ec4b4c5093f8e083ab7ff296138d02a31faf3b6c
Steven Soe <83544382+flyingdragon-codelab@users.noreply.github.com>
2022-05-23 17:43:40 +0800

    adding test variable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+final testVar = "This is test";
+
 GoogleSignIn _googleSignIn = GoogleSignIn(
   // Optional clientId
   // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
